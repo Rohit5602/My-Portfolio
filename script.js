79fc09e5f47bff550798835e076d49054bd8426e
Rohit5602 <109260218+Rohit5602@users.noreply.github.com>
2022-10-25 01:23:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Coder","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".name",{
+    strings:["","Rohit Maurya","Rohit Maurya"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
